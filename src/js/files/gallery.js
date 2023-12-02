dd1efda9022e51fb7901405fb900cddc6a40fabf
DenysO <3id3i19@gmail.com>
2023-12-02 03:07:57 +0100

    gallery need fix, added links to header

--- src/js/files/gallery.js
+++ src/js/files/gallery.js
@@ -26,7 +26,7 @@ import '@scss/libs/gallery/lightgallery.scss';
 // import '@scss/libs/gallery/lg-autoplay.scss';
 // import '@scss/libs/gallery/lg-zoom.scss';
 // import '@scss/libs/gallery/lg-pager.scss';
-// import '@scss/libs/gallery/lg-fullscreen.scss';
+import '@scss/libs/gallery/lg-fullscreen.scss';
 // import '@scss/libs/gallery/lg-share.scss';
 // import '@scss/libs/gallery/lg-comments.scss';s
 // import '@scss/libs/gallery/lg-rotate.scss';
@@ -44,7 +44,7 @@ if (galleries.length) {
 		galleyItems.push({
 			gallery,
 			galleryClass: lightGallery(gallery, {
-				// plugins: [lgZoom, lgThumbnail],
+				plugins: [ ],
 				licenseKey: '7EC452A9-0CFD441C-BD984C7C-17C8456E',
 				speed: 500,
 			})
